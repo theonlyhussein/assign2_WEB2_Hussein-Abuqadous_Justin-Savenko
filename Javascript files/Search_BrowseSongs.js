7010b6eeebee949fa5f1f6a433591aa868805d90
theonlyhussein <90483159+theonlyhussein@users.noreply.github.com>
2022-11-05 17:16:07 -0600

    radio button done

--- Javascript files/Search_BrowseSongs.js
+++ Javascript files/Search_BrowseSongs.js
@@ -17,46 +17,161 @@ document.addEventListener("DOMContentLoaded", function(event) {
 document.querySelector("#title").disabled = true;
 document.querySelector("#artist").disabled = true;
 document.querySelector("#genre").disabled = true;
+document.querySelector("#lessYear_button").disabled = true;
+document.querySelector("#greatYear_button").disabled = true;
+document.querySelector("#lessPOP_button").disabled = true;
+document.querySelector("#greatPOP_button").disabled = true;
+document.querySelector("#Less_Popularity").disabled = true;
+document.querySelector("#Greater_Popularity").disabled = true;
 document.querySelector("#less_Year").disabled = true;
-document.querySelector("#greater_Year").disabled=true;
-document.querySelector("#Less_Popularity").disabled=true;
-document.querySelector("#Greater_Popularity").disabled=true;
-document.querySelector("#lessYear_button").disabled=true;
-document.querySelector("#greatYear_button").disabled=true;
-document.querySelector("#lessPOP_button").disabled=true;
-document.querySelector("#greatPOP_button").disabled=true;
+document.querySelector("#greater_Year").disabled = true;
 });
 // Which radio button did the user choose? to unable the right input. to be continued ... 
 function verifyAnswer(){
   const radioButtons = document.querySelectorAll('input[name="fav"]');
   for (const radioButton of radioButtons) {
-    if (radioButton.clicked == true && radioButton.id == "title_button"){
+    if (document.getElementById(radioButton.id).checked && radioButton.id === "title_button"){
       document.querySelector("#title").disabled = false;
+      document.querySelector("#artist").disabled = true;
+      document.querySelector("#genre").disabled = true;
+      document.querySelector("#lessYear_button").disabled = true;
+      document.querySelector("#greatYear_button").disabled = true;
+      document.querySelector("#lessPOP_button").disabled = true;
+      document.querySelector("#greatPOP_button").disabled = true;
+      document.querySelector("#Less_Popularity").disabled = true;
+      document.querySelector("#Greater_Popularity").disabled = true;
+      document.querySelector("#less_Year").disabled = true;
+      document.querySelector("#greater_Year").disabled = true;
       break;
     }
-    else if (radioButton.clicked == true && radioButton.id == "artist_button"){
-      
+    if (document.getElementById(radioButton.id).checked && radioButton.id === "artist_button"){
       document.querySelector("#artist").disabled = false;
+      document.querySelector("#title").disabled = true;
+      document.querySelector("#genre").disabled = true;
+      document.querySelector("#lessYear_button").disabled = true;
+      document.querySelector("#greatYear_button").disabled = true;
+      document.querySelector("#lessPOP_button").disabled = true;
+      document.querySelector("#greatPOP_button").disabled = true;
+      document.querySelector("#Less_Popularity").disabled = true;
+      document.querySelector("#Greater_Popularity").disabled = true;
+      document.querySelector("#less_Year").disabled = true;
+      document.querySelector("#greater_Year").disabled = true;
       break; 
     }
-    else if (radioButton.clicked == true && radioButton.id == "genre_button"){
-      
+    if (document.getElementById(radioButton.id).checked && radioButton.id === "genre_button") {
       document.querySelector("#genre").disabled = false;
+      document.querySelector("#title").disabled = true;
+      document.querySelector("#artist").disabled = true;
+      document.querySelector("#lessYear_button").disabled = true;
+      document.querySelector("#greatYear_button").disabled = true;
+      document.querySelector("#lessPOP_button").disabled = true;
+      document.querySelector("#greatPOP_button").disabled = true;
+      document.querySelector("#Less_Popularity").disabled = true;
+      document.querySelector("#Greater_Popularity").disabled = true;
+      document.querySelector("#less_Year").disabled = true;
+      document.querySelector("#greater_Year").disabled = true;
       break;
     }
-    else if ( radioButton.id == "year_button"){
+    if (document.getElementById(radioButton.id).checked && radioButton.id === "year_button") {
+      document.querySelector("#genre").disabled = true;
+      document.querySelector("#title").disabled = true;
+      document.querySelector("#artist").disabled = true;
+      document.querySelector("#lessYear_button").disabled = false;
+      document.querySelector("#greatYear_button").disabled = false;
+      document.querySelector("#lessPOP_button").disabled = true;
+      document.querySelector("#greatPOP_button").disabled = true;
+      document.querySelector("#Less_Popularity").disabled = true;
+      document.querySelector("#Greater_Popularity").disabled = true;
+      document.querySelector("#less_Year").disabled = true;
+      document.querySelector("#greater_Year").disabled = true;
+      break;
+    }
 
-      document.querySelector("#lessYear_button").disabled=false;
-      document.querySelector("#greatYear_button").disabled=false;
+    if (document.getElementById(radioButton.id).checked && radioButton.id === "pop_button") {
+      document.querySelector("#genre").disabled = true;
+      document.querySelector("#title").disabled = true;
+      document.querySelector("#artist").disabled = true;
+      document.querySelector("#lessYear_button").disabled = true;
+      document.querySelector("#greatYear_button").disabled = true;
+      document.querySelector("#lessPOP_button").disabled = false;
+      document.querySelector("#greatPOP_button").disabled = false;
+      document.querySelector("#Less_Popularity").disabled = true;
+      document.querySelector("#Greater_Popularity").disabled = true;
+      document.querySelector("#less_Year").disabled = true;
+      document.querySelector("#greater_Year").disabled = true;
       break;
     }
-    else {
-      document.querySelector("#lessPOP_button").disabled=false;
-      document.querySelector("#greatPOP_button").disabled=false;
-      break;
-    }
-
     
+  }
+  }
+  function verifyAnswerYear(){
+    const radioButtons = document.querySelectorAll('input[name="year"]');
+    for (const radioButton of radioButtons) {
+      if (document.getElementById(radioButton.id).checked && radioButton.id === "lessYear_button"){
+        document.querySelector("#genre").disabled = true;
+      document.querySelector("#title").disabled = true;
+      document.querySelector("#artist").disabled = true;
+      document.querySelector("#lessYear_button").disabled = false;
+      document.querySelector("#greatYear_button").disabled = false;
+      document.querySelector("#lessPOP_button").disabled = true;
+      document.querySelector("#greatPOP_button").disabled = true;
+      document.querySelector("#Less_Popularity").disabled = true;
+      document.querySelector("#Greater_Popularity").disabled = true;
+      document.querySelector("#less_Year").disabled = false;
+      document.querySelector("#greater_Year").disabled = true;
+      break;
+      }
+
+      if (document.getElementById(radioButton.id).checked && radioButton.id === "greatYear_button"){
+        document.querySelector("#genre").disabled = true;
+        document.querySelector("#title").disabled = true;
+        document.querySelector("#artist").disabled = true;
+        document.querySelector("#lessYear_button").disabled = false;
+        document.querySelector("#greatYear_button").disabled = false;
+        document.querySelector("#lessPOP_button").disabled = true;
+        document.querySelector("#greatPOP_button").disabled = true;
+        document.querySelector("#Less_Popularity").disabled = true;
+        document.querySelector("#Greater_Popularity").disabled = true;
+        document.querySelector("#less_Year").disabled = true;
+        document.querySelector("#greater_Year").disabled = false;
+        break;
+      }
+
+  }
+  }
+
+  function verifyAnswerPop(){
+    const radioButtons = document.querySelectorAll('input[name="pop"]');
+    for (const radioButton of radioButtons) {
+      if (document.getElementById(radioButton.id).checked && radioButton.id === "lessPOP_button"){
+        document.querySelector("#genre").disabled = true;
+      document.querySelector("#title").disabled = true;
+      document.querySelector("#artist").disabled = true;
+      document.querySelector("#lessYear_button").disabled = true;
+      document.querySelector("#greatYear_button").disabled = true;
+      document.querySelector("#lessPOP_button").disabled = false;
+      document.querySelector("#greatPOP_button").disabled = false;
+      document.querySelector("#Less_Popularity").disabled = false;
+      document.querySelector("#Greater_Popularity").disabled = true;
+      document.querySelector("#less_Year").disabled = true;
+      document.querySelector("#greater_Year").disabled = true;
+      break;
+      }
+
+      if (document.getElementById(radioButton.id).checked && radioButton.id === "greatPOP_button"){
+        document.querySelector("#genre").disabled = true;
+        document.querySelector("#title").disabled = true;
+        document.querySelector("#artist").disabled = true;
+        document.querySelector("#lessYear_button").disabled = true;
+        document.querySelector("#greatYear_button").disabled = true;
+        document.querySelector("#lessPOP_button").disabled = false;
+        document.querySelector("#greatPOP_button").disabled = false;
+        document.querySelector("#Less_Popularity").disabled = true;
+        document.querySelector("#Greater_Popularity").disabled = false;
+        document.querySelector("#less_Year").disabled = true;
+        document.querySelector("#greater_Year").disabled = true;
+        break;
+      }
 
   }
   }
